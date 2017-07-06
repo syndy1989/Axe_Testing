a35674c605dd6db1acfcd8d92732f6be8289c34c
syndy1989 <sindujarajkumar@gmail.com>
2017-07-06 08:46:56 +0000

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM java8:jdk
+
+RUN mkdir C:\\axe
+
+WORKDIR C:\\axe
+
+COPY ./lib C:\\axe\\lib
+
+COPY Script.java C:\\axe\\Script.java
+
+COPY Parsing.java C:\\axe\\Parsing.java
+
+COPY Testing.java C:\\axe\\Testing.java
+
+COPY axe.min.js C:\\axe\\axe.min.js
+
+COPY phantomjs.exe C:\\axe\\phantomjs.exe
+
+COPY datafile.properties C:\\axe\\datafile.properties
+
+RUN javac -cp "./lib/*" Script.java
+
+ENTRYPOINT ["java"]
+
+CMD ["-cp","./lib/*;" ,"Script"]
+
---
+++ Script.java
@@ -0,0 +1,84 @@
+package docker;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.WebDriver;
+
+public class script{
+	
+	
+	static Testing t=new Testing();
+	static List<String> filelist =new ArrayList();
+	static ArrayList<JSONArray> final10=new ArrayList<JSONArray>();
+	static String result1=null;
+	static JSONArray jsonresults=null;
+	static ArrayList<String> pagename = new ArrayList<String>();
+	//Parsing value= new Parsing();
+	
+	static final URL scriptUrl = script.class.getResource("axe.min.js");
+
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, JSONException, ParseException{
+		
+		 File file = new File("C:\\axe\\datafile.properties");
+			FileInputStream fileInput = null;
+			try {
+				fileInput = new FileInputStream(file);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+			Properties prop = new Properties();
+			
+			//load properties file
+			try {
+				prop.load(fileInput);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			
+		
+		System.setProperty("phantomjs.binary.path", "C:\\axe\\phantomjs.exe");        
+
+		WebDriver driver = new PhantomJSDriver();
+		
+		driver.get(prop.getProperty("URL"));
+		
+		
+		//calling method axe
+		jsonresults=t.run_axe(driver, scriptUrl);
+		
+		System.out.println("Result"+jsonresults);
+		
+		//calling substring for unique file name
+		result1=t.Sub_String_url(driver.getCurrentUrl(), 1);
+		System.out.println("Result"+result1);
+		
+		
+		//creating file with unique name
+	String fileresult=	t.dynamic_filecreation(result1, jsonresults);
+		filelist.add(fileresult);
+		pagename.add("home");
+		
+		Parsing value=new Parsing();
+		
+		final10=value.parsingfile(filelist, pagename);
+		
+		System.out.println("Resultfinal10"+final10);
+		
+	}
+	
+		
+	
+}
+
